a8f26a1e0396d6ae3c85eac4a2c4fdda1c91c275
Vladimir <nettsundere@gmail.com>
2012-12-08 08:05:34 +0400

    We don't need to call downcase each time.

--- lib/redmine_profile_email_domain_whitelist/patches/user_patch.rb
+++ lib/redmine_profile_email_domain_whitelist/patches/user_patch.rb
@@ -11,11 +11,11 @@ module RedmineProfileEmailDomainWhitelist
 
       def validate_with_email_domain_check
         if mail.present? && mail_changed? && !admin?
-          mail_domain = mail.split("@")[-1]
+          mail_domain = Unicode.downcase(mail.split("@")[-1])
 
           p_s = Setting.plugin_redmine_profile_email_domain_whitelist
           whitelisted_domains = p_s['allowed_email_domains']
-          unless whitelisted_domains.any? { |d| Unicode.downcase(d) == Unicode.downcase(mail_domain) }
+          unless whitelisted_domains.any? { |d| Unicode.downcase(d) == mail_domain }
             mail_not_whitelisted_message = p_s['mail_domain_not_whitelisted_message']
             errors.add(:mail, mail_not_whitelisted_message)
           end
