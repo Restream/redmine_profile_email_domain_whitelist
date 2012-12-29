7cd81478bd6db5794affa8df992a8a4aa547596e
Vladimir <nettsundere@gmail.com>
2012-12-29 17:38:08 +0400

    Better test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,2 @@
-# Load the normal Rails helper
-helper_path = File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-if !File.exists?(helper_path)
-  helper_path = File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
-end
-
+helper_path = File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
 require helper_path
